bca27a78b94a689cc7adac6f4449289ea087eaf0
Igor <igor.crispimdiniz@gmail.com>
2018-09-19 23:08:03 -0300

    Tests for EventsData data container
    Tests rerenders on loading state changes and filtering of results by name.

--- src/containers/events-data.integ.spec.js
+++ src/containers/events-data.integ.spec.js
@@ -6,30 +6,50 @@ import ApolloClient from 'apollo-boost';
 import mockResolvesAfter from '../test-utils';
 
 describe('EventsData data fetch container tests', () => {
-  const client = new ApolloClient({
-    uri: 'http://dev.lets.events/graphql',
-  });
+  it('EventsData should fetch and return events data or an error', () => {
+    const client = new ApolloClient({
+      uri: 'http://dev.lets.events/graphql',
+    });
 
-  it('EventsData should fetch and return events data or an error', (done) => {
     const mockPromise = mockResolvesAfter(2 /* calls */, () => 'test');
     const functionChild = mockPromise.mock;
     render(
       <ApolloProvider client={client}>
-        <EventsData search="">{functionChild}</EventsData>
+        <EventsData>{functionChild}</EventsData>
       </ApolloProvider>,
     );
 
-    mockPromise.then(() => {
+    return mockPromise.then(() => {
       const firstCall = functionChild.mock.calls[0][0];
       const secondCall = functionChild.mock.calls[1][0];
 
-      try {
-        expect(firstCall.loading).toBe(true);
-        expect(!secondCall.loading && (!!secondCall.error || !!secondCall.data)).toBe(true);
-        done();
-      } catch (e) {
-        done().fail(e);
-      }
+      expect(firstCall.loading).toBeTruthy();
+      expect(!secondCall.loading && (secondCall.error || secondCall.data)).toBeTruthy();
+    });
+  });
+
+  it('EventsData should be able to filter results by its search query prop', () => {
+    const client = new ApolloClient({
+      uri: 'http://dev.lets.events/graphql',
+    });
+
+    const mockPromise = mockResolvesAfter(2 /* calls */, () => 'test');
+    const functionChild = mockPromise.mock;
+    const searchQuery = 'LatAm';
+    render(
+      <ApolloProvider client={client}>
+        <EventsData search={searchQuery}>{functionChild}</EventsData>
+      </ApolloProvider>,
+    );
+
+    return mockPromise.then(() => {
+      const secondCall = functionChild.mock.calls[1][0];
+
+      expect(secondCall.data.events.nodes).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ name: expect.stringContaining(searchQuery) }),
+        ]),
+      );
     });
   });
 });
