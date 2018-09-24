1084edd1e5f32087688651cdfc962f40cb21115a
Igor <igor.crispimdiniz@gmail.com>
2018-09-24 00:21:43 -0300

    Improved events list

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import { Flex } from 'grid-styled';
+import { FoldingCube } from 'styled-spinkit';
+
 import Header from 'components/header';
 import SearchBar from 'components/search-bar';
 import EventsData from 'containers/events-data';
-import { Flex } from 'grid-styled';
+import EventsList from 'components/events-list';
+import PaginationButtons from 'components/pagination-buttons';
+import ErrorCard from 'components/error-card';
 
 const MainPage = () => (
   <React.Fragment>
     <Header />
-    <Flex is="section" flexDirection="column" alignItems="center" mt="-2.5em">
+    <Flex is="section" flexDirection="column" alignItems="center" mt="-2.5em" mb="2.5em">
       <SearchBar>
         {({ searchValue }) => (
           <EventsData search={searchValue}>
-            {({ data }) => (
-              <div style={{ width: '100%' }}>
-                <ul>
-                  {data
-                    && data.events
-                    && data.events.nodes
-                    && data.events.nodes.map(event => <li key={event.id}>{event.name}</li>)}
-                </ul>
-              </div>
-            )}
+            {({
+              loading, data, error, retry, nextPage, previousPage,
+            }) => {
+              if (loading) {
+                return (
+                  <Flex pt="3em" style={{ height: '100vh' }}>
+                    <FoldingCube />
+                  </Flex>
+                );
+              }
+              if (error) return <ErrorCard errorMsg={error} buttonCallback={retry} />;
+              return (
+                data
+                && data.events
+                && data.events.nodes && (
+                  <React.Fragment>
+                    <h3>Best events around you</h3>
+                    <EventsList events={data.events.nodes} />
+                    <PaginationButtons nextCallback={nextPage} previousCallback={previousPage} />
+                  </React.Fragment>
+                )
+              );
+            }}
           </EventsData>
         )}
       </SearchBar>
